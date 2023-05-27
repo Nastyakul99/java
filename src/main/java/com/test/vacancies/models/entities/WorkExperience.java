88630a84cff3a972093b8df8bc74b86648e17e36
Anatasia <Anatasia.1999.kul@gmail.com>
2023-05-28 00:13:21 +0300

    added  <groupId>com.vladmihalcea</groupId>

--- src/main/java/com/test/vacancies/models/entities/WorkExperience.java
+++ src/main/java/com/test/vacancies/models/entities/WorkExperience.java
@@ -4,11 +4,19 @@ import java.time.Period;
 
 import javax.persistence.Embeddable;
 
+import org.hibernate.annotations.TypeDef;
+
+import com.vladmihalcea.hibernate.type.interval.PostgreSQLIntervalType;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@TypeDef(
+	    typeClass = PostgreSQLIntervalType.class,
+	    defaultForType = Period.class
+	)
 @Embeddable
 public class WorkExperience {
 	private Period minWorkExperience;
